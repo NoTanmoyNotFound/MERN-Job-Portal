ab87a56efb129f51c45a520289251f17d9fde709
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-03-04 09:42:55 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import ApplyJob from './pages/ApplyJob'
+import Applications from './pages/Applications'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/apply-job/:id' element={<ApplyJob />} />
+        <Route path='/applications' element={<Applications />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,15 @@
+const { createContext } = require("react");
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+    const value = {
+        
+    };
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    );
+};
