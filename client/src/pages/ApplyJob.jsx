8a57c277ce28f551a52775d43a7c4945e4747985
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-03-08 17:57:29 +0530

    build card logics

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import Loading from '../components/Loading'
+import Navbar from '../components/Navbar'
 
 const ApplyJob = () => {
-  return (
-    <div>
-      apply jobs
-    </div>
+
+  const {id} = useParams()
+
+  const [JobData, setJobData] = useState(null)
+
+  const {jobs} = useContext(AppContext)
+
+  const fetchJob = async () => {
+    const data = jobs.filter(job => job._id.toString() === id)
+    if(data.length !== 0){
+      setJobData(data[0])
+      console.log(data[0])
+    }
+  }
+
+  useEffect(()=>{
+    if(jobs.length > 0){
+      fetchJob()
+    }
+  },[id.jobs])
+
+  return JobData ? (
+    <>
+      <Navbar />
+    </>
+  ):(
+    <Loading />
   )
+
 }
 
 export default ApplyJob
