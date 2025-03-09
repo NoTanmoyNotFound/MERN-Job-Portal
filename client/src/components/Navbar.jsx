5cfb695eaf908f777778060c70ba0e94eb445dd1
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-03-09 12:12:33 +0530

    apply job hero

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <div className='shadow py-4'>
-      <div className='container px-4 2xl:px-20 mx auto flex justify-between items-center'>
+      <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
         <img src={assets.logo} className='h-9'/>
         {
           user
