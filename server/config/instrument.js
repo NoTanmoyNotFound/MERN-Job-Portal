64d6a408a5de39bf36e3cf7f428fc2ec3ac0725b
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-03-13 12:35:08 +0530

    removing main data

--- server/config/instrument.js
+++ server/config/instrument.js
@@ -3,4 +3,9 @@ import * as Sentry from "@sentry/node"
 
 Sentry.init({
   dsn: "https://372d2f40b202d5eb1321ddf4fdd8b9fe@o4508969080258560.ingest.us.sentry.io/4508969084387328",
+    integrations: [
+        Sentry.mongooseIntegration()
+    ],
+  
+  
 });
