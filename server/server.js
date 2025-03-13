4b965e89ae1af420ef67d4cdaab80d9b1aa27db0
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-03-13 21:35:15 +0530

    starting clerk auth

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node";
+import { clerkwebhooks } from './controllers/webhooks.js'
 
 
 //Initialize Express
@@ -22,6 +23,8 @@ app.get('/',(req,res)=> res.send("API Working"))
 app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   });
+  app.post('/webhooks',clerkwebhooks)
+  
 
 //Port
 const PORT = process.env.PORT || 5000
