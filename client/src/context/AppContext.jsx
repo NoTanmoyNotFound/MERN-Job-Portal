aff1ba0948f2df140492a04c0dbc0f5ab21400ed
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-03-15 18:46:04 +0530

    jobs sending backend to frontend

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { jobsData } from "../assets/assets";
 import axios from "axios";
-import { toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 
 export const AppContext = createContext();
 
@@ -21,7 +21,19 @@ export const AppContextProvider = (props) => {
 
     // Function to fetch jobs data
     const fetchJobs = async () => {
-        setJobs(jobsData);
+        try {
+            const {data} = await axios.get(backendUrl+'/api/jobs')
+
+            if (data.success) {
+                setJobs(data.jobs)
+                console.log(data.jobs)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+        
     };
 
     // Function to fetch company data
